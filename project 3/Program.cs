32eb9f37998c3175cc7167989521a8876fa6d736
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-08-30 23:55:32 +0300

    1

--- project 3/Program.cs
+++ project 3/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace project_3
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace project_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // 1 
+            int pages = 464;
+            bool isAvailable = true;
+            if ( pages > 300 && isAvailable)
+                {
+                Console.WriteLine("You can borrow this book");
+            }
         }
     }
 }
